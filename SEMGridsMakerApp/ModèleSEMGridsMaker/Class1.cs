34a84309277591af4dccebe6f6c7e6c18e730704
guilhemelias <guilhem.elias@gmail.com>
2021-04-08 16:52:55 +0200

    [DEV] Beginning of graphical implementation (WPF), with the menu and the Regular Step Program. Navigation between them enabled. Creation of the Arduino files folder.

---
+++ SEMGridsMakerApp/ModèleSEMGridsMaker/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ModèleSEMGridsMaker
+{
+    public class Class1
+    {
+    }
+}
---
+++ SEMGridsMakerApp/WpfApp1/ControlRegularStep.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SEMGridsMakerApp
+{
+    /// <summary>
+    /// Logique d'interaction pour ControlRegularStep.xaml
+    /// </summary>
+    public partial class ControlRegularStep : UserControl
+    {
+        public ControlRegularStep()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Menu_Click(object sender, RoutedEventArgs e)
+        {
+            ((ContentControl)this.Parent).Content = new ControlMenu();
+        }
+    }
+}
