8e0934beb98495d6a22a6bddc7dc6d8f556f7fa4
Guilhem Elias <guilhem.frederic.elias@cern.ch>
2021-05-10 11:23:52 +0200

    [DEV] Binding Python arduino to control the winder

--- SemGridsMakerPyQt/view/manager.py
+++ SemGridsMakerPyQt/view/manager.py
@@ -6,13 +6,12 @@ Created on Mon Apr 19 10:23:54 2021
 """
 
 from maker import Maker
-from json import JSONEncoder
-from semGrid import SemGrid
-import json
+from arduino import Arduino
 
 class Manager():
     def __init__(self):
         self.maker = Maker()
+        self.arduino =Arduino()
         self.myCurrentGrid=None
     
     def addSemGrid(self,name,seq,desc):
@@ -38,12 +37,45 @@ class Manager():
             if (grid.name == name):
                 return grid
         return False
+    
+    def getStepFromGrid(self,grid):
+        tabStep = []
+        for step in grid.sequence:
+            tabStep.append(step[0])
+        return tabStep
+    
+    def getLapFromGrid(self,grid):
+        tabLaps = []
+        for step in grid.sequence:
+            tabLaps.append(step[1])
+        return tabLaps
+
     def setCurrentGrid(self,grid):
         self.myCurrentGrid=grid
         
     def getMyCurrentGrid(self):
         return self.myCurrentGrid
     
+    def connectArduino(self):
+        portCom=self.maker.get_portCom()
+        
+        self.arduino.connect(portCom)
+        
+    def sendDataArduino(self,gap,index,tab1,tab2):
+        chain = str(index)
+        chain = chain+'/'+str(gap)
+        for elem in tab1:
+            chain = chain+'/'+str(elem)
+        for elem in tab2:
+            chain = chain+'/'+str(elem)
+        self.arduino.sendValue(chain) 
+            
+    def receiveValueArduino(self):
+        self.arduino.receivevalue()
+    
+    def closeArduino(self):
+        self.arduino.close()
+        
     
     
     
